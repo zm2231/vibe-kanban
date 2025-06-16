eca26240feca020b0ca6b61ebcdc5c9e5b88cfc4
Louis Knight-Webb <louis@bloop.ai>
2025-06-16 11:30:11 -0400

    Add click to component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ClickToComponent } from "click-to-react-component";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <ClickToComponent />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
