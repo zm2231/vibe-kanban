0654a66caf783e9b22dbb1376062ca79d9be5738
Louis Knight-Webb <louis@bloop.ai>
2025-06-17 21:15:43 -0400

    Task attempt e6b58807-7976-489f-ad8b-21018be694de - Final changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ClickToComponent } from "click-to-react-component";
+import { ThemeProvider } from "@/components/theme-provider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ClickToComponent />
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vibe-kanban-ui-theme">
+      <ClickToComponent />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
